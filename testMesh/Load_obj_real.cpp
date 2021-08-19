0ac80c75772dae3ab52d7e41955a66ab24914f66
Vitalik <maymeskul@gmail.com>
2021-08-19 14:19:09 +0300

    Добавьте файлы проекта.

---
+++ testMesh/Load_obj_real.cpp
@@ -0,0 +1,9 @@
+#include "Load_obj_real.h"
+
+Load_obj_real::Load_obj_real(char*)
+{
+}
+
+Load_obj_real::~Load_obj_real()
+{
+}
---
+++ testMesh/Load_obj_real.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+
+
+class Load_obj_real
+{
+private:
+public:
+	Load_obj_real(char*);
+	~Load_obj_real();
+};
